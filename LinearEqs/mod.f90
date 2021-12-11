de8d47825b51dcc3c594f02fe79ed1c2d65a7f49
suoraan <93644829+suoraan@users.noreply.github.com>
2021-12-11 13:34:47 +0800

    codes for linear Eqs

---
+++ LinearEqs/mod.f90
@@ -0,0 +1,10 @@
+module para
+	real,dimension(:,:),allocatable :: matrix,temp
+	integer:: typ,nx,ne,nn
+	integer :: i,k=1,ik
+	real,dimension(:),allocatable :: x,tempx,xx
+	real :: e = 2.718281828,n,sum,max
+	real*8 :: x1,x2,m,f,f1,f2
+	character :: r
+end module para
+
