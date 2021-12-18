ac88833a947b317272d27f0e2f087a201e273aca
Grace-ubuntu <hanshuanglvxue@sjtu.edu.cn>
2021-12-18 13:20:07 +0800

    done

--- PROJECT/P3.m
+++ PROJECT/P3.m
@@ -12,12 +12,3 @@ end
 x=[-1:0.01:1.0]
 y = 1./(1+25*x.^2)
 plot(x,y,'m');
-%求积分
-syms x;
-syms IN;
-IN = 0;
-for i = [2,3,4,5,6,7,8,9,10,11]
-   f1= (Ay(i)-Ay(i-1)).*(x-Ax(i))./(Ax(i)-Ax(i-1))+Ay(i);
-   I(i)= int(f1,x,Ax(i-1),Ax(i))
-   IN = IN +I(i)
-end
